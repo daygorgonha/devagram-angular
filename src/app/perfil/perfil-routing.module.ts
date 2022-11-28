d2fbdeafa7c64451fc850808ab4962d816cff8ac
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-11-28 19:34:49 -0300

    criacao do layout da tela de editar perfil

--- src/app/perfil/perfil-routing.module.ts
+++ src/app/perfil/perfil-routing.module.ts
@@ -1,8 +1,13 @@
+import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
 import { PerfilComponent } from './perfil.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'pessoal/editar',
+    component: EditarPerfilComponent
+  },
   {
     path: ':idUsuario',
     component: PerfilComponent
--- src/app/perfil/perfil.module.ts
+++ src/app/perfil/perfil.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { CompartilhadoModule } from './../compartilhado/compartilhado.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,17 +6,20 @@ import { CommonModule } from '@angular/common';
 import { PerfilRoutingModule } from './perfil-routing.module';
 import { PerfilComponent } from './perfil.component';
 import { CabecalhoPerfilComponent } from './cabecalho-perfil/cabecalho-perfil.component';
+import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
 
 
 @NgModule({
   declarations: [
     PerfilComponent,
-    CabecalhoPerfilComponent
+    CabecalhoPerfilComponent,
+    EditarPerfilComponent
   ],
   imports: [
     CommonModule,
     PerfilRoutingModule,
-    CompartilhadoModule
+    CompartilhadoModule,
+    ReactiveFormsModule
   ]
 })
 export class PerfilModule { }
