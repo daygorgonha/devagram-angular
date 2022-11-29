a4d24efd858c502607c6f86051b5556a6874f0e4
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-11-29 09:11:56 -0300

    ajustes

--- src/app/compartilhado/componentes/layout/navegacao/navegacao.component.ts
+++ src/app/compartilhado/componentes/layout/navegacao/navegacao.component.ts
@@ -1,11 +1,11 @@
-import { Router } from '@angular/router';
-import { ItemMenu } from './item-menu.type';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ItemMenu } from './item-menu.type';
 
 @Component({
   selector: 'app-navegacao',
   templateUrl: './navegacao.component.html',
-  styleUrls: ['./navegacao.component.scss']
+  styleUrls: ['./navegacao.component.scss'],
 })
 export class NavegacaoComponent implements OnInit {
   private rotaAtiva: string = 'home';
@@ -22,10 +22,23 @@ export class NavegacaoComponent implements OnInit {
       img: 'usuario',
       rotas: ['/perfil/pessoal', '/perfil/pessoal/editar'],
     },
-  }
-  constructor(private router: Router) { }
+  };
+  constructor(private router: Router, private rotaAtivada: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.rotaAtivada.url.subscribe(() => {
+      this.definirRotaAtiva();
+    });
+  }
+
+  public definirRotaAtiva() {
+    for (const menu in this.mapaDeRotas) {
+      const rotaMenu = this.mapaDeRotas[menu];
+      if (rotaMenu.rotas.includes(this.router.url)) {
+        this.rotaAtiva = menu;
+        break;
+      }
+    }
   }
 
   public redirecionarPara(menu: string): void {
@@ -37,12 +50,10 @@ export class NavegacaoComponent implements OnInit {
     const rotaMenu = this.mapaDeRotas[menu];
 
     let icone = rotaMenu.img;
-    if (rotaMenu.rotas.includes(this.router.url)
-      || this.rotaAtiva === 'menu') {
+    if (this.rotaAtiva === menu) {
       icone = `${rotaMenu.img}Ativo`;
-      this.rotaAtiva = menu;
     }
+
     return `assets/imagens/${icone}.svg`;
   }
-
 }
--- src/app/perfil/cabecalho-perfil/cabecalho-perfil.component.ts
+++ src/app/perfil/cabecalho-perfil/cabecalho-perfil.component.ts
@@ -1,6 +1,6 @@
 import { AutenticacaoService } from './../../compartilhado/autenticacao/autenticacao.service';
 import { DevagramUsuarioApiService } from './../../compartilhado/servicos/devagram-usuario-api.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { UsuarioDevagram } from './../../compartilhado/tipos/usuario-devagram.type';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -16,14 +16,17 @@ export class CabecalhoPerfilComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private rotaAtiva: ActivatedRoute,
     private servicoUsuario: DevagramUsuarioApiService,
     private servicoAutenticacao: AutenticacaoService
     ) {}
 
   ngOnInit(): void {
-    if(this.router.url === '/perfil/pessoal') {
+   this.rotaAtiva.url.subscribe(() => {
+    if (this.router.url === '/perfil/pessoal') {
       this.estaPerfilPessoal = true;
     }
+   });
   }
 
   public async manipularCliqueBotaoPrincipal(): Promise <void> {
@@ -46,6 +49,12 @@ export class CabecalhoPerfilComponent implements OnInit {
 
     try {
       await this.servicoUsuario.alternarSeguir(this.usuario._id);
+      if (this.usuario.segueEsseUsuario) {
+        this.usuario.seguidores--;
+      } else {
+        this.usuario.seguidores++;
+      }
+
       this.usuario.segueEsseUsuario = !this.usuario.segueEsseUsuario;
     } catch (e: any) {
       alert(e.error?.erro || 'Erro ao seguir/deixar de seguir o usuario!');
